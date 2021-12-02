e68e1480b0f1c01e03278d12792658279fa82432
martarho <S0cunPop>
2021-12-02 14:33:39 +0100

    remove error

--- 2021/data/Untitled-1.py
+++
@@ -1,8 +0,0 @@
-fh = open('day01_input.txt', 'r') # this opens a file and returns a filehandle
-
-mylist = list()
-for line in fh:
-    modified_line = int(line.rstrip())
-    mylist.append(modified_line)
-
-print(len(mylist))
