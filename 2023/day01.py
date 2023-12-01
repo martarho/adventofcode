36597369853d03d0a8d17339be7c9bf8ededa64e
martarho <mrodriguezorejuela@gmail.com>
2023-12-01 16:35:41 +0100

    Adding regex solution for part 2

--- 2023/day01.py
+++ 2023/day01.py
@@ -2,6 +2,7 @@ from lib import *
 from loguru import logger
 import typing
 import os
+import re
 
 
 def find_digits(s: str) -> list:
@@ -49,6 +50,40 @@ def problem2(f: typing.Union[str, os.PathLike]) -> None:
     print(total)
 
 
+def findandMatch(s):
+    d = {
+        "one": "1",
+        "two": "2",
+        "three": "3",
+        "four": "4",
+        "five": "5",
+        "six": "6",
+        "seven": "7",
+        "eight": "8",
+        "nine": "9",
+    }
+    # match all possibilities (substrings + generic \d for single-value integers)
+    # requires ?= lookahead for overlapping pattern matching
+    pattern = r"(?=(\d|one|two|three|four|five|six|seven|eight|nine))"
+    matches = re.findall(pattern, s)
+    digits = []
+    for m in matches:
+        if m.isdigit():
+            digits.append(m)
+        else:
+            digits.append(d[m])
+    return digits
+
+
+def problem2_approach2(f: typing.Union[str, os.PathLike]) -> None:
+    data = read_files(f)
+    total = 0
+    for s in data:
+        digits = findandMatch(s)
+        total += int(str(digits[0]) + str(digits[-1]))
+    print(total)
+
+
 logger.debug("--- Problem 1 - test")
 problem1("data/d01_test.txt")
 
@@ -60,3 +95,6 @@ problem2("data/d01_test2.txt")
 
 logger.debug("--- Problem 2")
 problem2("data/d01_part2.txt")
+
+logger.debug("--- Problem 2 w/ re")
+problem2_approach2("data/d01_part2.txt")
